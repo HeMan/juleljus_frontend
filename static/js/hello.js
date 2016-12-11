c8bcc9341e768fe683f80481d47f57ede0538360
Jimmy Hedman <jimmy.hedman@gmail.com>
2016-12-11 21:29:57 +0000

    Moved js and changed to relative url.
    - Moved javascript files to a separate directory.
    - Changed url's in the current javascript to use be relative.

--- static/js/hello.js
+++ static/js/hello.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $.ajax({
-        url: "http://[2002:5f6d:7d2e::5be]:8080/patterns"
+        url: "api/patterns"
     }).then(function(data) {
         data = $.parseJSON(data);
         $.each(data, function(i, item) {
@@ -18,6 +18,6 @@ $(document).ready(function() {
 
 function run_pattern(pattern) {
     $.ajax({
-        url: "http://[2002:5f6d:7d2e::5be]:8080/run/"+pattern
+        url: "api/run/"+pattern
     });
 };
